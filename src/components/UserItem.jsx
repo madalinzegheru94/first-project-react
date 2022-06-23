ec1f554bc7cc5834a7089f93e8e8780ff87e30c5
madalinzegheru94 <zegherumadalin@yahoo.com>
2022-06-23 14:19:13 +0300

    create the component UserItem.jsx

---
+++ src/components/UserItem.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class UserItem extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    
+    render (){
+    return (
+        <div>
+            <h3> {this.props.name} </h3>
+            <p>{this.props.email }</p>
+            
+            { this.props.isGoldClient 
+                ? <h3>Client GOLD</h3>
+                : null
+            }
+            {
+              this.props.isGoldClient
+                ? <img src='https://w7.pngwing.com/pngs/156/15/png-transparent-gold-diamond-golden-diamond-gold-diamond-thumbnail.png' alt='Gold Logo' width='30px' height='30px'/>
+                : <img src='https://static-bebeautiful-in.unileverservices.com/profile-icon-png-clip-art_22.png' alt='Black Logo' width='30px' height='30px'/>
+            }
+            <button id='delete-button' onClick={() => this.props.onDelete(this.props.id)} >Delete</button>
+            
+        </div>
+    );
+}
+}
+
+export default UserItem;
